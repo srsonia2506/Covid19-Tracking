fc5046978e012e474bc923fd086c58530080c6eb
Saravanan Sonia <srsonia98@yahoo.com.sg>
2020-03-24 21:44:56 +0800

    Add files via upload

--- website/app/routes.py
+++ website/app/routes.py
@@ -1,1 +1,11 @@
-#placeholder
+from app import app
+import parseCovid
+
+@app.route('/')
+@app.route('/title')
+
+def hello():
+    return parseCovid.parse()
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port='5000', debug=True)
